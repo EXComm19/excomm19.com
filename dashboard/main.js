39265f3391ecf86c71e2c17bbb8642fcbf6e68cd
EXComm19 <excomm19@icloud.com>
2019-07-18 23:46:00 +1000

    Add white overlay to unauth entry

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,12 +1,11 @@
-window.onload = function () {
-    console.log(document.cookie);
-    
+window.onload = function () {    
     if (document.cookie !== 'status=logged') {
         window.location = '/login'
+    } else {
+        document.getElementById('overlay').style.display = "none";
     }
 
     window.addEventListener("beforeunload", (e) => {
         document.cookie = "status=notLogged; expires=9999; path=/";
     })
-
 }
